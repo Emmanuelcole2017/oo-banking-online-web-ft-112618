2129575e2d78ce283eb9e8ab68f5f9983d8ebd8d
Emmanuel Cole <emmanuel.c2011@gmail.com>
2018-12-18 18:52:22 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -2,11 +2,15 @@ class Transfer
   # your code here
   attr_accessor :sender, :receiver, :amount, :status
 
-  def initialize(bank1, bank2, money)
-    @sender = bank1
-    @receiver = bank2
+  def initialize(account1, account2, money)
+    @sender = account1
+    @receiver = account2
     @amount = money
     @status = "pending"
   end
 
+  def valid?(account)
+    account.valid?
+  end
+  
 end
