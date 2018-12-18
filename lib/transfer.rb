32f5c9b7693bfb97d20f4d7881dc93a5a1acb3ab
Emmanuel Cole <emmanuel.c2011@gmail.com>
2018-12-18 19:19:59 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -18,11 +18,13 @@ class Transfer
   end
 
   def execute_transaction
-    if @status == "pending"
+    if @status == "pending" && @amount < @sender.balance
       @sender.balance -= @amount
       #@receiver.deposit(@amount)
       @receiver.balance += @amount
       @status = "complete"
+    else
+      "Transaction rejected. Please check your account balance."
     end
   end
 
