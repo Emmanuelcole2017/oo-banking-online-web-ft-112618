eb63cf1fd06cb8057bbada22463c7872a6555ea5
Emmanuel Cole <emmanuel.c2011@gmail.com>
2018-12-18 18:47:56 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,11 @@
 class Transfer
   # your code here
+  attr_accessor :account1, :account2, :amount
+
+  def initialize(bank1, bank2, money)
+    @account1 = bank1
+    @account2 = bank2
+    @amount = money
+  end
+  
 end
